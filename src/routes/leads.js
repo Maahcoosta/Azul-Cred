6d4d2945e124d4b2552b02e7408c8f7187e8cd38
maahcoosta <Maah.coosta@hotmail.com>
2025-12-15 17:18:41 -0300

    Adicione rotas de leads com operações CRUD para gerenciamento de leads. Simplificação.

---
+++ src/routes/leads.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const leadsController = require('../controllers/leadsController');
+
+router.post('/', leadsController.createLead);
+router.get('/:id', leadsController.getLeadById);
+router.put('/:id', leadsController.updateLead);
+router.delete('/:id', leadsController.deleteLead);
+
+module.exports = router;
+
